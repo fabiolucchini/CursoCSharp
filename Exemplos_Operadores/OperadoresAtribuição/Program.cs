826293ab87ee34c1592ddaedf801f09aeea048ba
fabiolucchini <59915755+fabiolucchini@users.noreply.github.com>
2022-09-21 16:09:20 -0300

    Add files via upload

---
+++ Exemplos_Operadores/OperadoresAtribuição/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresAtribuição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valorA = 10;
+            valorA += 10;
+            valorA -= 10;
+            valorA *= 3;
+            valorA /= 3;
+            valorA %= 4;
+
+            valorA = valorA + 10;
+            valorA = valorA - 10;
+            valorA = valorA * 10;
+            valorA = valorA / 10;
+            valorA = valorA % 10;
+
+
+        }
+    }
+}
---
+++ Exemplos_Operadores/OperadoresLogicos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sexo = "M";
+            int idade = 18;
+
+            if( sexo == "M" & idade == 18 )
+            {
+                Console.WriteLine("Deve se alistar");
+            }
+
+            
+            if (sexo == "M" || (sexo == "F" & idade == 18))
+            {
+
+            }
+
+            if( 10 == 10 & 10 > 9)
+            {
+
+            }
+            if( 10 == 10 | 10 == 9)
+            {
+
+            }
+
+            if ( 9 == 10 && 10 > 9)
+            {
+
+            }
+
+            if (10 == 10 || 10 > 9)
+            {
+
+            }
+        }
+    }
+}
