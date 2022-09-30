691240ef16dd5ad6b68b412d9224b6a1a1c13415
fabiolucchini <59915755+fabiolucchini@users.noreply.github.com>
2022-09-30 17:38:40 -0300

    Add files via upload

---
+++ POO_Exemplos/Exercicio_Criptografia/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Criptografia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Criptografia cripto = new Criptografia(CryptProvider.DES);
+            cripto.Key = "Curs0C$harp@2022!";
+            string texto = cripto.Encrypt("Teste da Aula C#");
+            Console.WriteLine("Texto:" + texto);
+            texto = cripto.Decrypt(texto);
+            Console.WriteLine("Texto:" + texto);
+        }
+    }
+}
