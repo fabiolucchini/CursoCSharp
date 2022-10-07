8eaf0d7d7ed38e4bdeab9c341088500b024300cb
fabiolucchini <59915755+fabiolucchini@users.noreply.github.com>
2022-10-06 21:38:19 -0300

    Add files via upload

---
+++ EF e WebApi/API_Alunos/API_Alunos/Models/Professor.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_Alunos.Models {
+    public class Professor {
+        [Key]        
+        public int ProfessorId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(80)]
+        public string email { get; set; }
+
+    }
+}
